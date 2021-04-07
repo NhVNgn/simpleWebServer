0a3d24b65b610e9256bbefff37b179bb379a429c
yca316 yca316 <yca316@sfu.ca>
2021-04-06 20:02:13 -0700

    Added main to Client.py

--- Client.py
+++ Client.py
@@ -31,12 +31,14 @@ CMD_OK = 'GET {}:{}/test.html HTTP/1.1\r\n\r\n'.format(HOST, Constants.SERVER_PO
 CMD_NOT_FOUND = 'GET {}:{}/dogg.html HTTP/1.1\r\n\r\n'.format(HOST, Constants.SERVER_PORT).encode()
 CMD_BAD_REQ = 'G@ETT {}:{}/test.html HTTP/1.1\r\n\r\n'.format(HOST, Constants.SERVER_PORT).encode()
 
-with socket(AF_INET, SOCK_STREAM) as clientSocket:
-    clientSocket.connect((HOST, Constants.SERVER_PORT))
-    clientSocket.send(CMD_OK)
 
-    while True:
-        data = clientSocket.recv(512)
-        if len(data) < 1:
-            break
-        print(data.decode(), end='')
+if __name__ == '__main__':
+    with socket(AF_INET, SOCK_STREAM) as clientSocket:
+        clientSocket.connect((HOST, Constants.SERVER_PORT))
+        clientSocket.send(CMD_OK)
+
+        while True:
+            data = clientSocket.recv(512)
+            if len(data) < 1:
+                break
+            print(data.decode(), end='')
