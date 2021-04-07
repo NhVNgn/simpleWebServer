29532eb51c1365e365b4419e85b52b78156a58ab
yca316 yca316 <yca316@sfu.ca>
2021-04-06 22:33:33 -0700

    Removed comments

--- Client.py
+++ Client.py
@@ -1,27 +1,3 @@
-# Define Server IP Address and Port
-# serverName = 'localhost'
-# serverPort = 12000
-# Build Server Address Using IP Address and Port
-# serverAddress=(serverName, serverPort)
-
-# Create UDP Socket for Client
-# clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-# This is the message received from the keyboard
-# message = input('Input lowercase sentence:')
-
-# Message sent to the Server
-# clientSocket.sendto(message.encode(), serverAddress)
-
-# Read reply characters from socket into string
-# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-
-# Print uot received string
-# print(modifiedMessage.decode())
-
-# Close the client socket
-# clientSocket.close()
-
 from socket import *
 import Constants
 
--- Server.py
+++ Server.py
@@ -3,39 +3,7 @@ from socket import *
 import Constants
 import sys
 import traceback
-# serverPort = 8000
-# def createServer():
-#     serverSocket = socket(AF_INET, SOCK_STREAM)
-#     try:
-#         serverSocket.bind(('localhost', serverPort))
-#         serverSocket.listen(5)
-#         while True:
-#             clientSocket, clientAddress = serverSocket.accept()
-#             message = clientSocket.recv(5000).decode()
-#             decoded_msg = message.split("\n")
-#             if (len(decoded_msg) > 0):
-#                 for i in range (0,len(decoded_msg)):
-#                     print("decoded msg: ", decoded_msg[i])
-#                 print("-------------------------------")
 
-#             fin = open('test.html')
-#             content = fin.read()
-#             fin.close()
-#             response = "HTTP/1.1 200 OK\r\n" + content
-#             clientSocket.sendall(response.encode())
-#             clientSocket.shutdown(SHUT_WR)
-
-#     except KeyboardInterrupt :
-#         print("\nShutting down...\n")
-#     except Exception as exc :
-#         print("Error:\n")
-#         print(exc)
-
-#     serverSocket.close()
-
-
-# print("Access localhost")
-# createServer()
 
 class Server:
     def __init__(self, port=Constants.SERVER_PORT):
